3c22c6207681338da885f1bedbdda4d85cb8b5cf
AHOYO Juste <justeahoyo@gmail.com>
2025-11-20 12:51:31 +0100

    Création de la page [groupId]

---
+++ client/app/pages/whatsapp/[groupId].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>WhatsApp Group: {{ groupId }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const groupId = computed(() => route.params.groupId)
+</script>
+
