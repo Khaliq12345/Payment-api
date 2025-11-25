c1cdc3c76e64f431035995a94d8a2e55c81679c4
AHOYO Juste <justeahoyo@gmail.com>
2025-11-25 13:03:54 +0100

    Ajout du bouton logout

--- client/app/components/Header.vue
+++ client/app/components/Header.vue
@@ -3,14 +3,30 @@
         :ui="{
             center: 'flex justify-center w-full',
             left: 'hidden',
-            right: 'hidden',
             container: 'flex items-center justify-center h-full',
         }"
     >
         <template #default>
-            <span class="font-bold text-xl">Paiement Groupe Whatsapp</span>
+            <div class="flex items-center gap-3">
+                <span class="font-bold text-xl">Paiement Groupe Whatsapp</span>
+
+                <UButton
+                    icon="i-heroicons-arrow-right-on-rectangle"
+                    color="error"
+                    variant="solid"
+                    @click="logout"
+                    class="ml-2"
+                >
+                </UButton>
+            </div>
         </template>
     </UHeader>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("expired_at");
+    navigateTo("/login");
+};
+</script>
